47c39f683646f9b40696e26f799907e6ee9db6de
gourav1100 <gauravb1100@gmail.com>
2024-02-19 02:53:36 +0530

    completed teams page.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import anime from 'animejs';
 import { Mutex } from 'async-mutex';
 import logo from '../../public/favicon.png';
@@ -11,27 +12,40 @@ import icon from '../../public/favicon.ico';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-    const helmetContext = {};
+    const [bias, setBias] = useState(0);
+    const [clientX, setClientX] = useState(0);
+    const [clientY, setClientY] = useState(0);
+
+    useEffect(() => {
+        anime({
+            targets: '#mouse-follower-dot',
+            marginLeft: clientX - 14,
+            marginTop: clientY - 14 + bias,
+            duration: 50,
+            easing: 'easeOutBack',
+        });
+        anime({
+            targets: '#mouse-follower-wrapper',
+            marginLeft: clientX - 16,
+            marginTop: clientY - 16 + bias,
+            duration: 150,
+            easing: 'easeOutBack',
+        });
+    }, [bias, clientX, clientY]);
     useEffect(() => {
         const mutex = new Mutex();
-        window.onmousemove = (e) =>
+        window.onmousemove = (e) => {
             mutex.runExclusive(() => {
-                const { clientX, clientY } = e;
-                anime({
-                    targets: '#mouse-follower-dot',
-                    marginLeft: clientX - 14,
-                    marginTop: clientY - 14,
-                    duration: 50,
-                    easing: 'easeOutBack',
-                });
-                anime({
-                    targets: '#mouse-follower-wrapper',
-                    marginLeft: clientX - 16,
-                    marginTop: clientY - 16,
-                    duration: 150,
-                    easing: 'easeOutBack',
-                });
+                setClientX(e.clientX);
+                setClientY(e.clientY);
             });
+        };
+        window.onscroll = () => {
+            mutex.runExclusive(() => {
+                setBias(parseInt(window.scrollY));
+            });
+        };
+        setBias(parseInt(window.scrollY));
     }, []);
     return (
         <html lang="en">
@@ -43,20 +57,27 @@ export default function RootLayout({ children }) {
                 />
                 <link rel="icon" sizes="64x64" href={icon.src} />
             </head>
-            <body className={inter.className + ' overflow-hidden'}>
+            <body
+                style={{
+                    background: '#181818',
+                }}
+                className={
+                    inter.className + ' overflow-x-hidden cursor-default'
+                }
+            >
                 <div
-                    className="p-3 rounded-full border border-teal-400 absolute shadow-inner shadow-teal-200 z-20"
+                    className="p-3 rounded-full border border-teal-400 absolute shadow-inner shadow-teal-200"
                     id="mouse-follower-wrapper"
                 ></div>
                 <div
-                    className="p-2 rounded-full absolute z-20"
+                    className="p-2 rounded-full absolute"
                     id="mouse-follower-dot"
                 >
                     <div className="p-1 rounded-full overflow-hidden bg-teal-400"></div>
                 </div>
                 <div
                     id="header"
-                    className="p-4 absolute w-full z-10 flex"
+                    className="p-4 w-full flex z-10 sticky top-0"
                     style={{ background: 'rgba(13, 13, 13, 0.8)' }}
                 >
                     <span>
--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,3 +1,129 @@
+import workBg from '../../../public/images/background/work-bg.jpg';
+import './style.css';
+
+const Teams = [
+    {
+        title: 'Chairperson',
+        team: [
+            {
+                name: 'Ranjeet Raj',
+                instagram: '',
+                linkedin: '',
+                image: workBg,
+            },
+            {
+                name: 'Ranjeet Raj',
+                instagram: '',
+                linkedin: '',
+                image: '',
+            },
+        ],
+    },
+    {
+        title: 'Chairperson',
+        team: [
+            {
+                name: 'Ranjeet Raj',
+                instagram: '',
+                linkedin: '',
+                image: workBg,
+            },
+            {
+                name: 'Ranjeet Raj',
+                instagram: '',
+                linkedin: '',
+                image: '',
+            },
+        ],
+    },
+];
 export default function Team() {
-    return <></>;
+    return (
+        <div id="font-bold">
+            <div
+                className="relative"
+                style={{
+                    height: '55vh',
+                }}
+            >
+                <div
+                    className="w-full absolute h-full -z-20"
+                    style={{
+                        backgroundImage: `url(${workBg.src})`,
+                        backgroundSize: 'cover',
+                    }}
+                ></div>
+                <div className="absolute bg-black bg-opacity-40 h-full w-full backdrop-blur-sm -z-10"></div>
+                <div className="absolute h-full w-full flex justify-center items-center">
+                    <div className="w-full flex justify-center flex-wrap mt-12">
+                        <div className="border border-pink-400 rounded-full p-4 md:p-5 text-pink-400 hover:bg-pink-500 transition-all hover:bg-opacity-50 flex items-center justify-center h-full">
+                            <i className="fi fi-sr-star text-2xl md:text-4xl flex h-full items-center justify-center"></i>
+                        </div>
+                        <div className="w-full h-fit mt-6 flex justify-center text-4xl md:text-5xl">
+                            Team Member
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="p-4 w-full -mt-8">
+                {Teams.map((teamObject, index) => {
+                    return (
+                        <div key={`team-${index}`} className="w-full mt-24">
+                            <div className="text-4xl md:text-5xl w-full flex justify-center">
+                                {teamObject.title}
+                            </div>
+                            <div className="w-full flex justify-center flex-wrap mt-8">
+                                {teamObject.team.map(
+                                    (teamMemberObject, personIndex) => {
+                                        return (
+                                            <div
+                                                className={`relative w-80 h-80 md:ml-8 lg:ml-16 group ${
+                                                    personIndex % 2
+                                                        ? 'mt-16'
+                                                        : 'mt-2'
+                                                }`}
+                                                key={`team-${index}-${personIndex}`}
+                                            >
+                                                <div className="group-hover:transition-all absolute border border-yellow-600 rounded-full w-full h-full group-hover:border-yellow-300 group-hover:shadow-inner group-hover:shadow-yellow-100"></div>
+                                                <div className="group-hover:transition-all absolute w-full h-full rounded-full bg-black bg-opacity-20 group-hover:bg-opacity-25 -z-10"></div>
+                                                <div className="group-hover:transition-all absolute w-full h-full rounded-full -ml-8 mt-8 group-hover:bg-black group-hover:bg-opacity-25 -z-10"></div>
+                                                <div
+                                                    className="group-hover:transition-all absolute w-full h-full rounded-full -ml-8 mt-8 -z-20 shadow-sm shadow-slate-400 group-hover:shadow-md group-hover:shadow-slate-300"
+                                                    style={{
+                                                        background: `url(${teamMemberObject.image.src})`,
+                                                        backgroundSize: 'cover',
+                                                    }}
+                                                ></div>
+                                                <div className="group-hover:transition-all hidden absolute w-full h-full rounded-full justify-center items-center group-hover:flex group-hover:flex-wrap">
+                                                    <a
+                                                        target="_blank"
+                                                        href={
+                                                            teamMemberObject.instagram
+                                                        }
+                                                    >
+                                                        <i className="fi fi-brands-instagram text-4xl text-pink-700 m-2 cursor-pointer"></i>
+                                                    </a>
+                                                    <a
+                                                        target="_blank"
+                                                        href={
+                                                            teamMemberObject.linkedin
+                                                        }
+                                                    >
+                                                        <i className="fi fi-brands-linkedin text-5xl m-2 text-blue-600 cursor-pointer"></i>
+                                                    </a>
+                                                </div>
+                                                <div className="w-full h-full flex items-end p-3">
+                                                    <span className="hover:transition-all p-3 pl-8 pr-8 cursor-pointer text-black rounded-full text-xl cardName hover:bg-gradient-to-r from-pink-500 to-amber-500">{`${teamMemberObject.name}`}</span>
+                                                </div>
+                                            </div>
+                                        );
+                                    },
+                                )}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
