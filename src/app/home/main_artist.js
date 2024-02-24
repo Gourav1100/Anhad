a3de85416d592e982aad0554be8030311c8a8689
pratham0039 <98754411+pratham0039@users.noreply.github.com>
2024-02-25 03:09:14 +0530

    Update main_artist.js

--- src/app/home/main_artist.js
+++ src/app/home/main_artist.js
@@ -1,8 +1,8 @@
 import image from "../../../public/images/mainArtist.jpg";
 
 const MainArtist = {
-    name: "Main Artist",
-    bio: "Artist Bio",
+    name: "Celebrity Releasing Soon",
+    bio: "We will release our main celebrity soon here on the website and our instagram handle, Get connected to us",
     instagram: "https://google.com",
     image: image.src,
 };
