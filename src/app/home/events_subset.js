5d24b41a1555bfefdcd541d040dc75caf13cd567
gourav1100 <gauravb1100@gmail.com>
2024-02-24 17:15:15 +0530

    added events to home page.

---
+++ src/app/home/events_subset.js
@@ -0,0 +1,35 @@
+import image from "../../../public/images/mainArtist.jpg";
+
+const Events = [
+    {
+        name: "Event 1",
+        link: "https://google.com",
+        image: image.src,
+        desc: "loren ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        name: "Event 1",
+        link: "https://google.com",
+        image: image.src,
+        desc: "loren ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        name: "Event 1",
+        link: "https://google.com",
+        image: image.src,
+        desc: "loren ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        name: "Event 1",
+        link: "https://google.com",
+        image: image.src,
+        desc: "loren ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        name: "Event 1",
+        link: "https://google.com",
+        image: image.src,
+        desc: "loren ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+];
+export default Events;
--- src/app/home/pages.js
+++ src/app/home/pages.js
@@ -6,6 +6,7 @@ import background from "../../../public/images/background/banner1.png";
 import music from "../../../public/images/background/music.png";
 import mainArtist from "./main_artist";
 import artists from "./artists";
+import events from "./events_subset";
 import "./style.css";
 
 export default function Home() {
@@ -82,7 +83,7 @@ export default function Home() {
                 <div className="w-full absolute h-full -z-20 bg-black bg-opacity-20"></div>
                 <div className="w-full h-full flex items-center absolute">
                     <div
-                        className="w-full opacity-70 hover:opacity-90 scale-105 hover:scale-110 transition-all duration-500 cursor-pointer -rotate-6 font-mono z-10 shadow-md shadow-black"
+                        className="w-full opacity-70 hover:opacity-90 scale-105 hover:scale-110 transition-all duration-500 cursor-pointer -rotate-6 text-oregano z-10 shadow-md shadow-black"
                         style={{ backgroundColor: "#181818" }}
                     >
                         <marquee
@@ -132,7 +133,7 @@ export default function Home() {
                 </div>
                 <div className="h-full flex items-center absolute z-0">
                     <div
-                        className="w-full opacity-70 hover:opacity-90 scale-105 hover:scale-110 transition-all duration-500 cursor-pointer rotate-12 font-mono shadow-md shadow-black"
+                        className="w-full opacity-70 hover:opacity-90 scale-105 hover:scale-110 transition-all duration-500 cursor-pointer rotate-12 text-oregano shadow-md shadow-black"
                         style={{ backgroundColor: "#181818" }}
                     >
                         <marquee
@@ -221,10 +222,10 @@ export default function Home() {
                     </span>
                 </div>
             </div>
-            <div className="w-full p-8 pb-2 text-oregano lg:p-64 lg:pt-8 lg:pb-2 artist-title text-3xl lg:text-6xl fade-in fadeInElement">
+            <div className="w-full p-8 pb-2 text-oregano lg:p-64 lg:pt-8 lg:pb-2 artist-title text-4xl lg:text-6xl fade-in fadeInElement">
                 Popular Categories
             </div>
-            <div className="w-full p-10 pt-2 text-oregano lg:p-72 lg:pt-2 lg:pb-8 text-2xl lg:text-3xl fade-in fadeInElement">
+            <div className="w-full p-10 pt-2 text-oregano lg:p-72 lg:pt-2 lg:pb-8 text-xl lg:text-3xl fade-in fadeInElement">
                 Singers and Bands at Anhad 2024
             </div>
             <div className="w-full p-10 pt-2 text-oregano lg:p-72 lg:pt-2 lg:pb-8 text-2xl lg:text-3xl fade-in fadeInElement flex justify-center flex-col lg:flex-row lg:flex-wrap">
@@ -267,6 +268,78 @@ export default function Home() {
                         </React.Fragment>
                     );
                 })}
+            </div>
+            <div className="relative overflow-hidden">
+                <div
+                    className="absolute h-full -z-20 opacity-50"
+                    style={{
+                        background:
+                            "linear-gradient(180deg, #FFC107 0%, #FF589B 100%, #FF589B 100%)",
+                        filter: "blur(250px)",
+                        width: "682px",
+                        bottom: "0px",
+                        insetInlineStart: "-358px",
+                    }}
+                ></div>
+                <div
+                    className="absolute h-full -z-20 opacity-70"
+                    style={{
+                        background:
+                            "linear-gradient(180deg, #5EE4C6 0%, #FF589B 100%, #2690FF 100%)",
+                        filter: "blur(250px)",
+                        width: "682px",
+                        bottom: "270px",
+                        right: "-200px",
+                    }}
+                ></div>
+                <div className="absolute bg-black bg-opacity-20 h-full w-full backdrop-blur-xl -z-10"></div>
+                <div className="w-full p-8 pb-2 lg:p-64 lg:pt-8 lg:pb-2 text-4xl lg:text-6xl fade-in fadeInElement flex justify-between">
+                    <span className="artist-title text-oregano">Events</span>
+                    <span
+                        onClick={() => window.open("/events", "_self")}
+                        className="z-10 cursor-pointer text-sm rounded-full p-2 lg:p-4 font-sans text-stone-300 border border-stone-300 hover:border-stone-100 hover:text-stone-100 transition-all duration-300 items-center"
+                    >
+                        View All Events
+                    </span>
+                </div>
+                <div className="w-full p-10 pt-2 text-oregano lg:p-72 lg:pt-2 lg:pb-8 text-xl lg:text-3xl fade-in fadeInElement">
+                    Events at Anhad
+                </div>
+                <div className="w-full p-10 pt-2 text-oregano lg:p-72 lg:pt-2 lg:pb-8 text-2xl lg:text-3xl fade-in fadeInElement flex justify-center flex-col lg:flex-row lg:flex-wrap">
+                    {events.map((event) => {
+                        return (
+                            <React.Fragment key={event.name}>
+                                <span
+                                    className="w-full lg:w-1/3 flex justify-center flex-wrap group relative p-2 cursor-pointer"
+                                    onClick={() =>
+                                        window.open(event.link, "_blank")
+                                    }
+                                >
+                                    <div className="w-96 overflow-hidden flex justify-center p-5">
+                                        <img
+                                            className="w-96 object-contain rounded-lg group-hover:scale-105 transition-all duration-300 z-10 cursor-pointer"
+                                            src={event.image}
+                                            alt={event.name}
+                                        />
+                                    </div>
+                                    <div className="absolute bottom-0 w-full h-0 rounded-xl z-10 opacity-0 group-hover:opacity-50 group-hover:h-full transition-all duration-300 bg-black">
+                                        <div className="w-full h-full flex items-center flex-col justify-end pb-5 pt-5">
+                                            <span className="text-white text-2xl">
+                                                {event.name}
+                                            </span>
+                                            <span className="text-white text-lg text-ellipsis overflow-hidden w-full pr-16 pl-16 pt-2 line-clamp-2">
+                                                {event.desc}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className="absolute top-10 left-14 h-10 w-10 rounded-full z-20 bg-white opacity-15 group-hover:opacity-100 group-hover:bg-gradient-to-tr from-pink-500 to-yellow-500 transition-all duration-300 text-center items-center">
+                                        <i className="text-pink-500 group-hover:text-white text-sm fi fi-rs-arrow-up-right"></i>
+                                    </div>
+                                </span>
+                            </React.Fragment>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
