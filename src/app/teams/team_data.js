1ea4ce671116a4b66ebae7fbf845f2edbedfa6a0
Vishu9354 <vishu0883386@gmail.com>
2024-02-28 12:58:20 +0530

    update in team page

--- src/app/teams/team_data.js
+++ src/app/teams/team_data.js
@@ -1,7 +1,7 @@
 import abhishekVerma from '../../../public/images/team/Academic.jpg';
 import vikasGarg from '../../../public/images/team/Vikas.jpg';
 import snehaAhuja from '../../../public/images/team/Sneha.jpg';
-import parval from '../../../public/images/team/parval.jpeg';
+import parval from '../../../public/images/team/praval.jpeg';
 import partham from "../../../public/images/team/partham.png"
 import tanishaKhare from '../../../public/images/team/Tanisha.jpg';
 import shivamU from '../../../public/images/team/ShivamU.jpg';
@@ -20,6 +20,9 @@ import manish from '../../../public/images/team/Jaishwar.jpg';
 
 import priyanka from '../../../public/images/team/priyanka.jpg';
 import ayush from '../../../public/images/team/ayush.jpg';
+import raghav from '../../../public/images/team/raghav.jpg';
+import rishabh from '../../../public/images/team/rishabh.jpg';
+// import satyam from '../../../public/images/team/satyam.jpg';
 const Team = [
     {
         title: 'Chairperson',
@@ -187,92 +190,33 @@ const Team = [
                 team_name: 'Event Managment(Cultral)',
                 instagram: 'https://www.instagram.com/ayushraj.2003?igsh=OWplZGxzdXh6ZTIw',
                 linkedin: 'https://www.linkedin.com/in/ayush-raj-4aa621255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-                image: ayush,
+                image:ayush,
             },
             {
-                name: 'Manish Jaiswar',
+                name: 'Satyam Raj',
                 team_name: 'Event Management (Cultural)',
-                instagram: 'https://www.instagram.com/manish.j.2.0',
+                instagram: 'https://www.instagram.com/satyamraj21.9?igsh=cW9oZDJxMDB0OGN5',
                 linkedin:
-                    'https://www.linkedin.com/in/manish-jaiswar-783495230',
-                image: manish,
+                    'https://www.linkedin.com/in/satyam-raj-b26b2325b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+                image: '#',
             },
             {
-                name: 'Pranshu Jaiswal',
-                team_name: 'Event Management (Technical)',
-                instagram: 'https://www.instagram.com/pranshu__jaiswal/',
+                name: 'Pranshu JaiswalRaghav Jaiman ',
+                team_name: 'PR & Outreach team ',
+                instagram: 'https://www.instagram.com/mr.raghavjaiman?igsh=cHNma3JranExamRv',
                 linkedin:
-                    'https://www.linkedin.com/in/pranshu-jaiswal-388139230/',
-                image: pranshuJaiswal,
+                    'https://www.linkedin.com/in/raghav-jaiman-137285265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+                image: raghav,
             },
             {
-                name: 'AMIT RAJPUT',
-                team_name: 'Security Team',
-                instagram: 'https://www.instagram.com/amit.rajput360/',
-                linkedin: 'linkedin.com/in/amit-rajput-48a065230',
-                image: amitRajput,
-            },
-            {
-                name: 'SHIVAM SAURABH',
-                team_name: 'Security Team',
-                instagram: 'https://www.instagram.com/s_h_i_v_a_msaurabh',
-                linkedin: 'https://www.linkedin.com/in/shivam-saurabh74',
-                image: shivamSaurabh,
-            },
-            {
-                name: 'Prashant Singh',
-                team_name: 'Public Relations and Outreach Team',
-                instagram: 'https://www.instagram.com/_prashant._.singh',
+                name: 'Rishabh Sharma',
+                team_name: 'PR & Outreach team',
+                instagram: 'https://www.instagram.com/_rishabh___21?igsh=cGQ0cTl6Y2gwaWcy',
                 linkedin:
-                    'https://www.linkedin.com/in/prashant-singh-079886272',
-                image: prashantSingh,
+                    'https://www.linkedin.com/in/rishabh-sharma-7325702ab?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+                image:rishabh,
             },
-            {
-                name: 'Aditya Saidawat',
-                team_name: 'Public Relation and Outreach Team',
-                team_position: 'Head',
-                instagram: 'https://www.instagram.com/aditya_saidawat',
-                linkedin: 'www.linkedin.com/in/aditya-saidawat-383412281',
-                image: aditya,
-            },
-            {
-                name: 'Vatsal Tyagi',
-                team_name: 'Sponsorship',
-                team_position: 'Team Head',
-                instagram: 'NA',
-                linkedin: 'https://www.linkedin.com/in/vatsal-tyagi-3a0264237/',
-                image: vatsal,
-            },
-            {
-                name: 'Sanath Naveen Sharma',
-                team_name: 'Sponsorship',
-                team_position: 'Head',
-                instagram: 'NA',
-                linkedin: 'NA',
-                image: sanath,
-            },
-            {
-                name: 'Vansh Singh',
-                team_name: 'Hospitality',
-                instagram: 'https://www.instagram.com/rathore.vansh_5005',
-                linkedin: 'https://www.linkedin.com/in/vansh-singh-77988b235',
-                image: vanshSingh,
-            },
-            {
-                name: 'Priyanshu Mishra',
-                team_name: 'Hospitality',
-                instagram: "Instagram Account doesn't exist",
-                linkedin: 'www.linkedin.com/in/priyanshumishra31072003',
-                image: priyanshuMishra,
-            },
-            {
-                name: 'PRIYANSHU CHAUDHARY',
-                team_name: 'Media Team',
-                instagram: 'https://www.instagram.com/pc_zone__here',
-                linkedin:
-                    'https://www.linkedin.com/in/priyanshu-chaudhary-4aa7b1223',
-                image: priyanshuChaudhary,
-            },
+
         ],
     },
 ];
